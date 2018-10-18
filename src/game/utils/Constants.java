77310d6b9561323c86dae5868db821884a0fdc1b
SathishChanda <nanisani007@gmail.com>
2018-10-18 00:09:33 -0400

    Resources package created. Moved world map into resources package.

--- src/game/utils/Constants.java
+++ src/game/utils/Constants.java
@@ -1,8 +1,9 @@
 package game.utils;
+
 public class Constants {
 
     public static final String RISK_GAME_NAME = "Risk Game";
-    public static final String FILE_DOMAIN_PATH = "src/game/";
+    public static final String FILE_DOMAIN_PATH = "src/resources/";
     public static final String MAP_FILE_NAME = "world.map";
     public static final String MAP_KEY = "[Map]";
     public static final String CONTINENTS_KEY = "[Continents]";
@@ -10,5 +11,6 @@ public class Constants {
     public static final String MAP_KEY_PATTERN = "^[a-zA-Z]+=[a-zA-Z.-]+.[a-zA-Z]$";
     public static final String CONTINENTS_KEY_PATTERN = "^[a-zA-Z ]+=[0-9]$";
     public static final String TERRITORIES_KEY_PATTERN = "";
+    public static final String USER_MAP_FILE_NAME = "UserMap.map";
 
 }
