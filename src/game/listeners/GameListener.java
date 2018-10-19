1f9e28ff343610c6430e14111881662bb5f13738
venkat <venkat.ch0153@gmail.com>
2018-10-19 00:47:27 -0400

    java_doc_model

--- src/game/listeners/GameListener.java
+++ src/game/listeners/GameListener.java
@@ -1,19 +1,47 @@
 package game.listeners;
 
+/**
+ * Listen the various method in the game
+ * interface between various classes
+ */
 public interface GameListener {
 
+    /**
+     * Listen the mapValidity
+     */
     void onMapLoadSuccess();
 
+    /**
+     * Listen when the loading map is invalid
+     * @param message, error message for failure
+     */
     void onMapLoadFailure(String message);
 
+    /**
+     * Listen when territory list is valid in map file
+     */
     void onTerritoryMapValid();
 
+    /**
+     * Listen when territory data in invalid
+     * @param message, error message for failure
+     */
     void onTerritoryMapInvalid(String message);
 
+    /**
+     * Listen when continent list is valid in map file
+     */
     void onContinentMapValid();
 
+    /**
+     * triggers when continent data is invalid
+     * @param message, error message for failure
+     */
     void onContinentMapInvalid(String message);
 
+    /**
+     * Listen when map is saved successfully
+     */
     void onUserMapSaveSuccess();
 
 }
