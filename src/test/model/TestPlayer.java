b422f7150e2ea7f8933d1b01fdc52aab31c28726
venkat <venkat.ch0153@gmail.com>
2018-11-01 18:25:33 -0400

    testing of number of armies to be assigned according to risk rules

--- src/test/model/TestPlayer.java
+++ src/test/model/TestPlayer.java
@@ -19,26 +19,14 @@ public class TestPlayer {
     }
 
     @Test
-    public void testArmiesForPlayernum1() {
-        player = new Player(2);
-        assertEquals(35, player.getArmyNum());
-
-    }
-
-    @Test
     public void testPlayerId() {
         assertEquals(1, player.getPlayerID());
     }
 
     @Test
-    public void testAddcountry() {
-        assertEquals(30, territory.getArmyNum());
-    }
-
-    @Test
     public void testArmiesForPlayernum2() {
     player = new Player(2);
-        assertEquals(35, player.getArmyNum());
+        assertEquals(40, player.getArmyNum());
     }
     @Test
     public void testArmiesForPlayernum3() {
@@ -48,17 +36,17 @@ public class TestPlayer {
     @Test
     public void testArmiesForPlayernum4() {
      player = new Player(4);
-        assertEquals(35, player.getArmyNum());
+        assertEquals(30, player.getArmyNum());
     }
     @Test
     public void testArmiesForPlayernum5() {
      player = new Player(5);
-        assertEquals(35, player.getArmyNum());
+        assertEquals(25, player.getArmyNum());
     }
     @Test
     public void testArmiesForPlayernum6() {
      player = new Player(6);
-        assertEquals(35, player.getArmyNum());
+        assertEquals(20, player.getArmyNum());
     }
 
 }
