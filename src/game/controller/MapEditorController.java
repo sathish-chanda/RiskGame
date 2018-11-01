3d76a9e96ac6821e5bd99a94c61b2adb9c60f535
prashant <prashantraj.hongal@gmail.com>
2018-10-31 20:25:38 -0400

    Code Refactored 1. MadEditor UI elements

--- src/game/controller/MapEditorController.java
+++ src/game/controller/MapEditorController.java
@@ -2,6 +2,8 @@ package game.controller;
 
 import game.model.Continent;
 import game.model.RiskModel;
+import game.model.Territory;
+import game.utils.LogHelper;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -10,6 +12,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
@@ -20,6 +23,7 @@ public class MapEditorController implements Initializable {
     private RiskModel riskModel;
     private Map<String, String> mapComponents;
     private List<Continent> continentList;
+    private List<Territory> territoryList;
     @FXML
     private TextField authorTextField;
     @FXML
@@ -38,6 +42,13 @@ public class MapEditorController implements Initializable {
     private ListView continentsListView;
     @FXML
     private ComboBox continentsComboBox;
+    @FXML
+    private ListView territoriesListView;
+    @FXML
+    private ComboBox territoriesComboBox;
+    @FXML
+    private ListView adjacentTerritoriesListView;
+
 
     /**
      * Constructor of class {@link MapEditorController}
@@ -51,6 +62,13 @@ public class MapEditorController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setDataToUserInterfaceElements();
+        setViewElementsListeners();
+    }
+
+    private void setViewElementsListeners() {
+        //continentsListView.setOnMouseClicked(event -> loadTerritoriesListView(continentsListView.getSelectionModel().getSelectedIndex()));
+        continentsComboBox.setOnAction(event -> loadTerritoriesListView(continentsComboBox.getSelectionModel().getSelectedIndex()));
+        territoriesComboBox.setOnAction(event -> loadAdjacentTerritoriesListView(territoriesComboBox.getSelectionModel().getSelectedIndex()));
     }
 
     /**
@@ -64,6 +82,7 @@ public class MapEditorController implements Initializable {
         wrapTextField.setText(mapComponents.get("wrap"));
         loadContinentsListView();
         loadContinentsComboBox();
+        loadTerritoriesComboBox();
     }
 
 
@@ -86,9 +105,47 @@ public class MapEditorController implements Initializable {
     }
 
     /**
-     *
+     * Method to load territories into list view
      */
-    private void loadTerritoriesListView() {
+    private void loadTerritoriesListView(int position) {
+        territoriesListView.getItems().clear();
+        List<Territory> territoriesList = continentList.get(position).getTerritoryList();
+        for (Territory territory : territoriesList) {
+            territoriesListView.getItems().add(territory.getTerritoryName());
+        }
+    }
+
+    /**
+     * Method to load territories into combo box
+     */
+    private void loadTerritoriesComboBox() {
+        prepareTerritoriesList();
+        for (Territory territory : territoryList) {
+            territoriesComboBox.getItems().add(territory.getTerritoryName());
+        }
+    }
+
+    /**
+     * Method to create territories list from existing continent list
+     */
+    private void prepareTerritoriesList() {
+        territoryList = new ArrayList<>();
+        for (Continent continent : continentList) {
+            territoryList.addAll(continent.getTerritoryList());
+        }
+    }
+
+    /**
+     * Method to load adjacent territories into list view
+     */
+    private void loadAdjacentTerritoriesListView(int position) {
+        adjacentTerritoriesListView.getItems().clear();
+        if (position >= 0) {
+            List<String> adjacentTerritoriesList = territoryList.get(position).getAdjacentCountryList();
+            for (String adjacentTerritory : adjacentTerritoriesList) {
+                adjacentTerritoriesListView.getItems().add(adjacentTerritory);
+            }
+        }
 
     }
 
