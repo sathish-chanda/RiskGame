7ca9c3a9608046fef045cc4ed4b8448b209fc67a
prashantraj <prashantraj.hongal@gmail.com>
2018-11-05 10:56:23 -0500

    Player Select class added

--- src/game/controller/PlayerSelectController.java
+++ src/game/controller/PlayerSelectController.java
@@ -1,16 +1,31 @@
 package game.controller;
 
 import game.model.RiskModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class PlayerSelectController implements ActionListener {
 
-    RiskModel riskModel;
+    @FXML
+    Button selectPlayerButton;
+    @FXML
+    ComboBox selectPlayerComboBox;
+
 
     @Override
     public void actionPerformed(ActionEvent event) {
 
     }
+
+    /*private void init() {
+        selectPlayerButton.setOnAction(event -> );
+    }*/
+
+
+    //private void
+
 }
---
+++ src/game/main/PlayerSelect.java
@@ -0,0 +1,20 @@
+package game.main;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
+
+public class PlayerSelect implements EventHandler<ActionEvent> {
+
+
+    @Override
+    public void handle(ActionEvent event) {
+
+    }
+
+
+
+}
