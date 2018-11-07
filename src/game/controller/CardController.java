c98cf3f69d820f5a358477b7328ae8af001d6f0d
prashantraj <prashantraj.hongal@gmail.com>
2018-11-07 17:14:30 -0500

    Card View class removed.

--- src/game/controller/CardController.java
+++ src/game/controller/CardController.java
@@ -1,29 +1,28 @@
 package game.controller;
 
 import game.model.CardModel;
-import game.view.CardView;
+import game.utils.LogHelper;
 import game.model.Player;
 
+import java.util.Observable;
 import java.util.Observer;
 
 /**
  * This class creates the observable pattern of players card
  */
-public class CardController extends Object {
+public class CardController {
     private CardModel cardModel;
-    private CardView cardView;
 
     /**
      * constructor class where initializes the observer and observable pattern
      */
     public CardController() {
         cardModel = new CardModel();
-        cardView = new CardView();
-        cardModel.addObserver(cardView);
     }
 
     /**
      * This method initiates the exchange card for the players
+     *
      * @param player
      */
     public void exchangeCardsForArmies(Player player) {
